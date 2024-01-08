b646c8ac4ac43203f84b307f4d66b48664fc0b51
BirinVlad <90749149+BirinVlad@users.noreply.github.com>
2024-01-08 14:42:50 +0200

    Laba4.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,27 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String word = "Pilip"; //тут можна змінити слово
+        if(PalindromChecker.isPalindrome(word)){
+            System.out.println("Це слово палідром");
         }
+        else System.out.println("Це слово не палідром");
+    }
+}
+class PalindromChecker {
+    public static boolean isPalindrome(String word){
+        word = word.toLowerCase().replaceAll("\\s+", "");
+        int left = 0;
+        int right = word.length() - 1;
+
+        while (left < right) {
+            if(word.charAt(left) != word.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 }
